27e0e5fbc81d5d7e84eaca894ba828fc74612677
Silly-Joe <josefb1997@gmail.com>
2025-07-12 20:18:45 +0200

    move vertex into own module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod vertex;
+use vertex::Vertex;
+
 use std::sync::Arc;
 
 use wgpu::util::DeviceExt;
@@ -6,24 +9,6 @@ use winit::{
     event_loop::EventLoop,
     window::{Window, WindowAttributes},
 };
-
-#[repr(C)]
-#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 2],
-}
-
-impl Vertex {
-    const ATTRIBS: [wgpu::VertexAttribute; 1] = wgpu::vertex_attr_array![0 => Float32x2];
-
-    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &Self::ATTRIBS,
-        }
-    }
-}
 
 #[derive(Default)]
 struct App {
---
+++ src/vertex.rs
@@ -0,0 +1,17 @@
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct Vertex {
+    pub position: [f32; 2],
+}
+
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 1] = wgpu::vertex_attr_array![0 => Float32x2];
+
+    pub fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
